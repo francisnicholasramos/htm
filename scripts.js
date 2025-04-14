2872a86cc0f86115df81d0299c736b279f690f7c
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-04-14 21:52:38 +0800

    Done

--- scripts.js
+++ scripts.js
@@ -3,30 +3,33 @@ let library = [];
 addBookToLibrary(
   "Cracking The Coding Interview",
   "Gayle Laakmann McDowell",
-  "320"
+  "350",
+  "read"
 );
 
-const books = document.querySelector(".books-section");
+const bookSectionDisplay = document.querySelector(".books-section");
 const form = document.querySelector("form");
-
-const removeBtn = document.querySelector(".exit");
-
-console.log(`Array of books:`, library);
 
 function Book(title, author, pages, readStatus) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readStatus = "not read";
+  this.readStatus = readStatus;
 }
 
 function addBookToLibrary(title, author, pages, readStatus) {
-  library.push(new Book(title, author, pages, readStatus));
+  const newAddedBook = new Book(title, author, pages, readStatus);
+  library.push(newAddedBook);
+  console.log("Add:", library);
+  return newAddedBook;
 }
 
-function deleteArr(index) {
-  library = library.filter((item) => item.id !== index);
+console.log(`Array count:`, library);
+
+function deleteBook(id) {
+  library = library.filter((item) => item.id !== id);
+  console.log("Delete:", library);
 }
 
 form.addEventListener("submit", (e) => {
@@ -34,11 +37,8 @@ form.addEventListener("submit", (e) => {
   const bookTitle = document.querySelector("#title").value;
   const bookAuthor = document.querySelector("#author").value;
   const bookPages = document.querySelector("#pages").value;
+  const bookStatus = document.querySelector("#status").value;
   const form = document.querySelector("form");
-
-  //if (bookTitle === "" && bookAuthor === "") {
-  //  return;
-  //}
 
   const card = document.createElement("div");
   const exit = document.createElement("button");
@@ -49,13 +49,19 @@ form.addEventListener("submit", (e) => {
   const title = document.createElement("h3");
   const author = document.createElement("p");
   const pages = document.createElement("p");
+  const readStatus = document.createElement("p");
 
   card.classList.add("card");
   exit.classList.add("exit");
+
+  const book = addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+  exit.dataset.bookId = book.id;
+
   exit.onclick = () => {
-    deleteArr(book.id);
-    displayBook();
+    deleteBook(book.id);
+    card.remove(); // removes the card from the DOM
   };
+
   cardImage.classList.add("image-card");
   bookContent.classList.add("book-content");
   title.classList.add("title");
@@ -63,23 +69,24 @@ form.addEventListener("submit", (e) => {
   author.classList.add("author");
   pages.classList.add("pages");
 
-  books.appendChild(card);
+  bookSectionDisplay.appendChild(card);
   card.appendChild(exit);
   card.appendChild(cardImage);
   cardImage.appendChild(image);
   image.src =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM21oVe-TtjmXAEMEWaAVUKZDmg9CIu64IYw&s";
-  // "https://cdn.kobo.com/book-images/f68de379-e763-441c-8159-a949ea575237/1200/1200/False/the-subtle-art-of-not-giving-a-f-ck.jpg"
   card.appendChild(bookContent);
   bookContent.appendChild(title);
   bookContent.appendChild(author);
   bookContent.appendChild(pages);
+  bookContent.appendChild(readStatus);
 
-  exit.textContent = "x";
+  exit.innerHTML = "&#10006;";
   title.textContent = bookTitle;
   author.textContent = bookAuthor;
   pages.textContent = `${bookPages} pages`;
-  addBookToLibrary(bookTitle, bookAuthor, bookPages);
+  readStatus.textContent = bookStatus;
+
   form.reset();
 });
 
@@ -98,6 +105,7 @@ function displayBook() {
     const title = document.createElement("h3");
     const author = document.createElement("p");
     const pages = document.createElement("p");
+    const readStatus = document.createElement("p");
 
     card.classList.add("card");
     exit.classList.add("exit");
@@ -107,9 +115,9 @@ function displayBook() {
     author.classList.add("author");
     pages.classList.add("pages");
 
-    exit.textContent = "x";
+    exit.innerHTML = "&#10006;";
     exit.onclick = () => {
-      deleteArr(book.id);
+      deleteBook(book.id);
       displayBook();
     };
     image.src =
@@ -118,6 +126,7 @@ function displayBook() {
     title.title = book.title;
     author.textContent = book.author;
     pages.textContent = `${book.pages} pages`;
+    readStatus.textContent = book.readStatus;
 
     books.appendChild(card);
     card.appendChild(exit);
@@ -127,12 +136,8 @@ function displayBook() {
     bookContent.appendChild(title);
     bookContent.appendChild(author);
     bookContent.appendChild(pages);
+    bookContent.appendChild(readStatus);
   });
 }
 
 displayBook();
-
-//TODO:
-//
-// 1. refactor rendering content using map
-// 2. scrape an image copy image address for card image
